9fd68dd3ba3f9ca1a9162f19a821d24b5f46053d
Gerges hanna FCI-H <Gerges hanna FCI-H@DESKTOP-I4APBG9>
2019-11-06 17:56:53 +0200

    Project Started (Product)

---
+++ src/Product/ProductClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Product;
+
+/**
+ *
+ * @author Gerges hanna FCI-H
+ */
+public class ProductClass {
+    
+}
