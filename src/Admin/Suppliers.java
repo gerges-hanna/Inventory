0c19452e772a59845a1787845e79620125b8937d
bebo <bebo@192.168.1.6>
2019-11-09 16:19:27 +0200

    up4

---
+++ src/Admin/Suppliers.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Admin;
+import Product.Files;//To use Files Class
+/**
+ *
+ * @author Bebo
+ */
+public class Suppliers extends Product.FatherClass{
+    private String SupplierAddress;
+    private int ContactNumber;
+    private String SupplierEmail;
+    private String category;
+    private String Query;
+    final private String filepath="/Users/salib/file/Supplier.txt";//Final variable can't be modifed
+
+    public String getcategory() {
+        return category;
+    }
+
+    public void setcategory(String Category) {
+        this.category = Category;
+    }
+    public String getSupplierAddress() {
+        return SupplierAddress;
+    }
+
+    public void setSupplierAddress(String SupplierAddress) {
+        this.SupplierAddress = SupplierAddress;
+    }
+
+    public int getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setContactNumber(int ContactNumber) {
+        this.ContactNumber = ContactNumber;
+    }
+
+    public String getSupplierEmail() {
+        return SupplierEmail;
+    }
+
+    public void setSupplierEmail(String SupplierEmail) {
+        this.SupplierEmail = SupplierEmail;
+    }
+Files obj1=new Files();//obj1 from file class
+//Supplier Query:ID@Name@lastname@Address@contact_number@Email@category
+  public void AddInFileSupplier()
+     {
+        
+                obj1.write("my first text", filepath, true);
+      
+     }
+}
