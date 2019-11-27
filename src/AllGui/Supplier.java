d2a905336ea5432a2570736c5a69f024e0470839
bebo <bebo@192.168.1.6>
2019-11-27 14:41:55 +0200

    update 6

---
+++ src/AllGui/Supplier.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AllGui;
+
+import java.awt.Color;
+import javax.swing.*;
+
+/**
+ *
+ * @author bebo
+ */
+public class Supplier extends Admin.Suppliers {
+       public JPanel run()
+    {
+    JPanel p=new JPanel();
+    p.setBackground(Color.blue);
+    return p;
+}
+    
+}
