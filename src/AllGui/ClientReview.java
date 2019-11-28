4a2bafe8859f551a566004cc10f9bd5c01103866
John <John@DESKTOP-487L0CC.lan>
2019-11-28 15:56:11 +0200

    Salereview update

---
+++ src/AllGui/ClientReview.java
@@ -0,0 +1,88 @@
+package AllGui;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.border.Border;
+
+public class ClientReview extends Client.ClientClass{
+    
+    public JPanel run(){
+       
+       //Create
+    JTextField searchBarCR = new JTextField();
+    
+    
+    JPanel panelClientReview = new JPanel();
+
+    JButton buttonClientReview = new JButton("Search");
+    JButton backButtonCR = new JButton("Back");
+    
+    String clientreviewcb[]={};
+    JComboBox cbCR = new JComboBox(clientreviewcb);
+    
+    JLabel lbTotalCR = new JLabel("Total Sales:");
+    JLabel lbProfitCR = new JLabel("Total Profit:");
+    JLabel lbSearchBarCR = new JLabel("Search:");
+    
+    String tableDataCR[][]= new String[100][2];
+    String tableColumnCR[]={"1","2"};
+    
+    JTable tableClientReview=new JTable(tableDataCR,tableColumnCR);
+    JScrollPane spCR=new JScrollPane(tableClientReview);
+    
+    //BOUNDS
+    backButtonCR.setBounds(25,25,70,30);
+    lbSearchBarCR.setBounds(170, 39, 150, 50);
+    searchBarCR.setBounds(270,45,350,30);
+    cbCR.setBounds(635, 45, 200, 30);
+    buttonClientReview.setBounds(850,45,100,30);
+    tableClientReview.setBounds(100,100,1000,400);
+    spCR.setBounds(100,130,1000,400);
+    lbTotalCR.setBounds(25, 570, 150, 50);
+    lbProfitCR.setBounds(300, 570, 150, 50);
+    
+   //fonts
+   lbSearchBarCR.setFont(new Font("Tahoma",0,25));
+    searchBarCR.setFont(new Font("Tahoma",0,20));
+    cbCR.setFont(new Font("Tahoma",0,20));
+    buttonClientReview.setFont(new Font("Tahoma",0,20));
+    tableClientReview.setFont(new Font("Tahoma",0,20));
+    lbTotalCR.setFont(new Font("Tahoma",0,25));
+    lbProfitCR.setFont(new Font("Tahoma",0,25)); 
+    
+    
+    //colors
+    panelClientReview.setBackground(Color.white);
+    searchBarCR.setBackground(Color.white);
+    cbCR.setBackground(Color.white);
+    tableClientReview.setBackground(Color.LIGHT_GRAY);
+    
+    
+    
+    panelClientReview.setLayout(null);   
+    panelClientReview.setSize(1200, 700); 
+    panelClientReview.setVisible(true);
+    
+    
+    
+    //ADD
+    panelClientReview.add(spCR);
+    panelClientReview.add(searchBarCR);
+    panelClientReview.add(cbCR);
+    panelClientReview.add(buttonClientReview);
+    panelClientReview.add(tableClientReview);
+    panelClientReview.add(lbTotalCR);
+    panelClientReview.add(lbProfitCR);
+    panelClientReview.add(lbSearchBarCR);
+    panelClientReview.add(backButtonCR);
+    
+    
+    
+    return panelClientReview;
+    
+    }
+    
+    
+}
--- src/AllGui/SaleReview.java
+++ src/AllGui/SaleReview.java
@@ -1,33 +1,93 @@
 package AllGui;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.border.Border;
 public class SaleReview extends Client.ClientClass{
+    private Border Border;
     
     
    public JPanel run(){
+       
+       //Create
     JTextField searchBar = new JTextField();
-    JPanel panel = new JPanel();
+    
     
     JPanel panelsalereview = new JPanel();
 
-    JButton buttonsalereview = new JButton("Search");
+    JButton buttonSaleReview = new JButton("Search");
+    JButton backButton = new JButton("Back");
     
     String salereviewcb[]={};
     JComboBox cbsr = new JComboBox(salereviewcb);
     
-    JLabel lbTotal = new JLabel();
-    JLabel lbProfit = new JLabel();
+    JLabel lbTotal = new JLabel("Total Sales:");
+    JLabel lbProfit = new JLabel("Total Profit:");
+    JLabel lbSearchBar = new JLabel("Search:");
     
-    String tableData[]={};
-    String tableColumn[]={};
-    
-    
-    
+    String tableData[][]=new String[100][2];
+    String tableColumn[]={"1","2"};
     
     
+
+  
+           
+              
     
-    panelsalereview.setBackground(Color.cyan);
+    
+    JTable tableSaleReview=new JTable(tableData,tableColumn);
+     JScrollPane sp=new JScrollPane(tableSaleReview); 
+     
+    
+    
+    
+    //BOUNDS
+    backButton.setBounds(25,25,70,30);
+    lbSearchBar.setBounds(170, 39, 150, 50);
+    searchBar.setBounds(270,45,350,30);
+    cbsr.setBounds(635, 45, 200, 30);
+    buttonSaleReview.setBounds(850,45,100,30);
+    tableSaleReview.setBounds(100,100,1000,400);
+    sp.setBounds(100,130,1000,400);
+    lbTotal.setBounds(25, 570, 150, 50);
+    lbProfit.setBounds(300, 570, 150, 50);
+    
+   //fonts
+   lbSearchBar.setFont(new Font("Tahoma",0,25));
+    searchBar.setFont(new Font("Tahoma",0,20));
+    cbsr.setFont(new Font("Tahoma",0,20));
+    buttonSaleReview.setFont(new Font("Tahoma",0,20));
+    tableSaleReview.setFont(new Font("Tahoma",0,20));
+    lbTotal.setFont(new Font("Tahoma",0,25));
+    lbProfit.setFont(new Font("Tahoma",0,25)); 
+    
+    
+    //colors
+    panelsalereview.setBackground(Color.white);
+    searchBar.setBackground(Color.white);
+    cbsr.setBackground(Color.white);   
+    tableSaleReview.setBackground(Color.LIGHT_GRAY);
+    
+    
+    
+    panelsalereview.setLayout(null);
+    panelsalereview.setSize(1200, 700); 
+    panelsalereview.setVisible(true);
+    
+    
+    
+    panelsalereview.add(searchBar);
+    panelsalereview.add(cbsr);
+    panelsalereview.add(buttonSaleReview);
+    panelsalereview.add(sp);
+    panelsalereview.add(lbTotal);
+    panelsalereview.add(lbProfit);
+    panelsalereview.add(lbSearchBar);
+    panelsalereview.add(backButton);
+    panelsalereview.setSize(1200,700);
+    
     
     
     return panelsalereview;
