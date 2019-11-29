956132d8fbd4c7963f6586452b7fd274b81ac2ff
Gerges hanna FCI-H <Gerges hanna FCI-H@DESKTOP-I4APBG9>
2019-11-29 14:13:11 +0200

    update 101

--- src/AllGui/Register.java
+++ src/AllGui/Register.java
@@ -49,8 +49,8 @@ public class Register extends Client.ClientClass{
         JTextField ph = new  JTextField();
         JButton b1 = new JButton("Submit");
         JButton b2 = new JButton("Log in");
-        JCheckBox box=new JCheckBox("Show PassWord");
-        box.setBounds(fx+433,fy+105,120,20);
+        JCheckBox box=new JCheckBox("");
+        box.setBounds(fx+405,fy+105,18,14);
         box.addActionListener(new ActionListener() {
                 public void actionPerformed (ActionEvent e){
                 
