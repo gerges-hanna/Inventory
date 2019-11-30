6519f986d7f7e3912659e1c6421d2642ca9c8601
Andrew-PC <Andrew-PC@Andrew>
2019-11-30 13:54:55 +0200

    update delete function v2

--- src/Product/Files.java
+++ src/Product/Files.java
@@ -197,51 +197,37 @@ public class Files
                 }
                 break;
             case "Category.txt":
-                if(pros.size() == 1)
+                for(int i = 0; i < pros.size(); i++)
                 {
-                    write("", filePath, false);
-                }
-                else
-                {
-                    for(int i = 0; i < pros.size(); i++)
+                    if(((Admin.Categories)pros.get(i)).getCategory().equals(id))
+                        continue;
+                    else
                     {
-                        if(((Admin.Categories)pros.get(i)).getCategory().equals(id))
-                            continue;
-                        else
-                        {
-                            Query = ((Admin.Categories)pros.get(i)).getCategory() + "@"
-                                    + ((Admin.Categories)pros.get(i)).getCategoryDescription();
-                        }
-                        write(Query, filePath, append);
-                        append = true;
+                        Query = ((Admin.Categories)pros.get(i)).getCategory() + "@"
+                                + ((Admin.Categories)pros.get(i)).getCategoryDescription();
                     }
-                    break;
+                    write(Query, filePath, append);
+                    append = true;
                 }
+                break;
             case "Supplier.txt":
             //Supplier Query:ID@Name@LName@SupplierAddress@ContactNumber@SupplierEmail@Category
-                if(pros.size() == 1)
+                for(int i = 0; i < pros.size(); i++)
                 {
-                    write("", filePath, false);
-                }
-                else
-                {
-                    for(int i = 0; i < pros.size(); i++)
+                    if( ((Admin.Suppliers)pros.get(i)).getID().equals(id))
+                        continue;
+                    else
                     {
-                        if( ((Admin.Suppliers)pros.get(i)).getID().equals(id))
-                            continue;
-                        else
-                        {
-                            Query = ((Admin.Suppliers)pros.get(i)).getID() + "@" +
-                                    ((Admin.Suppliers)pros.get(i)).getName() + "@"+
-                                    ((Admin.Suppliers)pros.get(i)).getLName() + "@" +
-                                    ((Admin.Suppliers)pros.get(i)).getSupplierAddress()
-                                    + "@" + ((Admin.Suppliers)pros.get(i)).getContactNumber()
-                                    + "@" + ((Admin.Suppliers)pros.get(i)).getSupplierEmail()
-                                    + "@" + ((Admin.Suppliers)pros.get(i)).getCategory() + "@";
-                        }
-                        write(Query, filePath, append);
-                        append = true;
+                        Query = ((Admin.Suppliers)pros.get(i)).getID() + "@" +
+                                ((Admin.Suppliers)pros.get(i)).getName() + "@"+
+                                ((Admin.Suppliers)pros.get(i)).getLName() + "@" +
+                                ((Admin.Suppliers)pros.get(i)).getSupplierAddress()
+                                + "@" + ((Admin.Suppliers)pros.get(i)).getContactNumber()
+                                + "@" + ((Admin.Suppliers)pros.get(i)).getSupplierEmail()
+                                + "@" + ((Admin.Suppliers)pros.get(i)).getCategory() + "@";
                     }
+                    write(Query, filePath, append);
+                    append = true;
                 }
             case "Login.txt":
                 // Order;userName;password;case;
@@ -278,3 +264,4 @@ public class Files
         return s;
     }
 }
+//
