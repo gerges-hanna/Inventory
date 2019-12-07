03ab29345464e73ed2aa795fddc2fa27b8ed95d1
user <user@LAPTOP-4B0RD431>
2019-12-07 23:17:10 +0200

    up 7

--- src/Admin/GraphClass.java
+++ src/Admin/GraphClass.java
@@ -10,21 +10,44 @@ import java.util.ArrayList;
 
 public class GraphClass
 {
-    private final Files f = new Files();
-    private final ArrayList<Integer> categoryQuantities;
-    private final ArrayList<String> categoryNames;
+    private final  Files f = new Files();
+    private final  ArrayList<Integer> categoryQuantities;
+    private final  ArrayList<String> categoryNames;
+    
     public GraphClass()
     {
+        
         ArrayList<Object> dataReaded = f.read("sellReview.txt");
         categoryQuantities = new ArrayList<>();
         categoryNames = new ArrayList<>();
         for(int i = 0; i < dataReaded.size(); i++)
         {
-            int x = ((FatherClass)dataReaded.get(i)).getQuantity();
-            String x2 = ((FatherClass)dataReaded.get(i)).getCategory();
-            categoryQuantities.add(x);
-            categoryNames.add(x2);
+            if(categoryNames.isEmpty())
+            {
+                categoryNames.add( ((FatherClass)dataReaded.get(i)).getCategory()  );
+                categoryQuantities.add(  ((FatherClass)dataReaded.get(i)).getQuantity() );
+            }
+            else
+            {
+                boolean added = false;
+                for(int j = 0; j < categoryNames.size(); j++)
+                {
+                    if( categoryNames.get(j).equals( ((FatherClass)dataReaded.get(i)).getCategory()  ) )
+                    {
+                        categoryQuantities.set(j,  categoryQuantities.get(j) + ((FatherClass)dataReaded.get(i)).getQuantity() );
+                        added = true;
+                    }
+                    
+                }
+                if(!added)
+                {
+                    categoryNames.add( ((FatherClass)dataReaded.get(i)).getCategory()  );
+                    categoryQuantities.add(  ((FatherClass)dataReaded.get(i)).getQuantity() );
+                }
+            }
         }
+        
+        
     }
 
     public ArrayList<Integer> getCategoryQuantities()
--- src/AllGui/Report.java
+++ src/AllGui/Report.java
@@ -4,22 +4,25 @@
  * and open the template in the editor.
  */
 package AllGui;
-
+/*
 import Admin.GraphClass;
 import java.awt.*;
+import java.io.File;
 import javax.swing.*;
-/*import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;*/
-
+import org.jfree.data.category.DefaultCategoryDataset;
+*/
 /**
  *
  * @author user
  */
-public class Report extends Admin.GraphClass{
+
+/*
+public class Report  {
     
     public JPanel Run(){
         
@@ -28,8 +31,17 @@ public class Report extends Admin.GraphClass{
         JButton bBack=new JButton("Back");
        f.ButtonBackHome(bBack);
        
-       /*
+       
+       // To Make Variable From GraphClass
+       GraphClass gc = new GraphClass();
+       
+       //Make For Loop To Add Values
        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+       for(int i = 0; i< gc.getCategoryQuantities().size(); i++)
+       {
+           dataset.setValue(gc.getCategoryQuantities().get(i),"Quantity Sold From",gc.getCategoryNames().get(i));      
+       }
+       
        
         JFreeChart chart = ChartFactory.createBarChart3D("Graph Category",
                 "Category", 
@@ -40,16 +52,15 @@ public class Report extends Admin.GraphClass{
                 true,
                 true);
         chart.setBackgroundPaint(Color.lightGray);
-        chart.getTitle().setPaint(Color.blue);
+        chart.getTitle().setPaint(Color.BLUE);
         chart.setBorderPaint(Color.GRAY);
         CategoryPlot plot = chart.getCategoryPlot();
         plot.setRangeGridlinePaint(Color.GREEN);
-        ChartFrame frame = new ChartFrame("Count", chart);
+        ChartFrame frame = new ChartFrame("Graph For Category", chart);
         frame.setVisible(true);
         frame.setSize(1200,700);
         frame.setResizable(false);
         
-        */
         
         
         p.add(bBack);
@@ -61,3 +72,4 @@ public class Report extends Admin.GraphClass{
     }
     
 }
+*/
